e4674c00e8a3a8b57f1d5e1344379be34810a188
sharan98 <rudreshsharan@gmail.com>
2019-11-20 17:57:18 +0530

    Use the container deployed on an EC2 of the ECS cluster

--- src/api/index.js
+++ src/api/index.js
@@ -28,7 +28,9 @@ const movies = [
 const fetchMoies = async () => {
   // const response = movies;
   // const response = await axios.get(`${URL}/${comicNumber}`);
-  const response = await axios.get(`http://13.232.172.185:8000/getMovies`);
+  const response = await axios.get(
+    `http://ec2-3-80-8-109.compute-1.amazonaws.com:8000/getMovies`
+  );
   if (response.status >= 400) {
     throw new Error(response);
   }
